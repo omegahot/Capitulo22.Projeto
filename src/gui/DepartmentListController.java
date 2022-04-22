d6613c3b7b59591a54ab95612f7a7de5b05fd48c
omegahot <leo_omegahot@hotmail.com>
2022-04-22 11:46:08 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,15 +36,18 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
-	public synchronized void loadView(String absoluteName) {
+	public synchronized <T> void loadView(String absoluteName, Consumer<T> initializinAction) {
 		
 		try {
 			
@@ -63,44 +67,19 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().clear(); // Clear para limpar todos os filhos.
 			mainVBox.getChildren().add(mainMenu); // add os novos filhos no node.
 			mainVBox.getChildren().addAll(newVbox.getChildren()); // add uma coleção do objeto newVBox.
+			
+			// Massete para não precisar criar um novo loadView para cada chamada de tela.
+			T controller = loader.getController();
+			initializinAction.accept(controller);
 			
 		} catch (IOException e) {
 			Alerts.alerts("Error", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
 
-	public synchronized void loadView2(String absoluteName) {
-		
-		try {
-			
-			// Carregando a tela filha. 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVbox = loader.load();
-			
-			/* Criando um objeto novo mainScene e criando um objeto VBox.
-			 * Referenciando o scrollpane (principal) fazendo um casting de ScrollPane.
-			 * Referenciando o VBox fazendo um casting de VBox
-			 * */
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			// Criando um objeto Node recebendo o objeto filho na primeira posição do vetor.
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear(); // Clear para limpar todos os filhos.
-			mainVBox.getChildren().add(mainMenu); // add os novos filhos no node.
-			mainVBox.getChildren().addAll(newVbox.getChildren()); // add uma coleção do objeto newVBox.
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-			
-		} catch (IOException e) {
-			Alerts.alerts("Error", null, e.getMessage(), AlertType.ERROR);
-		}
-	}
 	
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {			
+	public void initialize(URL url, ResourceBundle rb) {
 	}
 
 }
